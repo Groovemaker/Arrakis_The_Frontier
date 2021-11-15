5841b83c06f8c67be0f53901999d7fa2c8061cc5
Groove <63636914+Groovemaker@users.noreply.github.com>
2021-11-15 17:47:28 +0100

    Update cl_scoreboard.lua

--- gamemodes/arrakis/gamemode/cl_scoreboard.lua
+++ gamemodes/arrakis/gamemode/cl_scoreboard.lua
@@ -1,37 +1,43 @@
 if CLIENT then
-    local Scoreboard_Roundness = 8
-	local Scoreboard_Color = Color(220, 220, 220, 220)
-	local Scoreboard_XGap = 6
-	local Scoreboard_YGap = 6
-	local Scoreboard_TitleToNamesGap = 4
+    local Scoreboard_Roundness = 2
+	local Scoreboard_Color = Color(35, 35, 35, 111)
+	local Scoreboard_XGap = 9
+	local Scoreboard_YGap = 9
+	local Scoreboard_TitleToNamesGap = 2
 
-	local Title_Height = 12
-	local Title_Color = Color(255, 160, 000, 255)
-	local Title_Font = "ScoreboardTitleFont"
-	local Title_Text = "Fancy Tastic Gmod Server"
-	local Title_BackgroundRoundness = 4
-	local Title_BackgroundColor = Color(80, 80, 80, 200)
+	local Title_Height = 1
+	local Title_Color = Color(255, 255, 255, 111)
+	local Title_Font = "Scoreboarder"
+	local Title_Text = ""
+	local Title_BackgroundRoundness = 2
+	local Title_BackgroundColor = Color(35, 35, 35, 56)
 
 	local Players_Spacing = 4
 	local Players_EdgeGap = 4
-	local Players_BackgroundRoundness = 4
-	local Players_BackgroundColor = Color(160, 160, 160, 220)
+	local Players_BackgroundRoundness = 2
+	local Players_BackgroundColor = Color(35, 35, 35, 56)
 
 	local PlayerBar_Height = 24
-	local PlayerBar_Color = Color(000, 000, 000, 255)
-	local PlayerBar_Font = "ScoreboardPlayersFont"
-	local PlayerBar_BackgroundRoundness = 4
-	local PlayerBar_BackgroundColor = Color(220, 220, 220, 255)
+	local PlayerBar_Color = Color(255, 255, 255, 255)
+	local PlayerBar_Font = "Scoreboarder"
+	local PlayerBar_BackgroundRoundness = 2
+	local PlayerBar_BackgroundColor = Color(35, 35, 35, 56)
 
 	local InfoBar_Height = 32
-	local InfoBar_Color = Color(255, 160, 000, 255)
-	local InfoBar_Font = "ScoreboardInfoFont"
-	local InfoBar_BackgroundRoundness = 4
-	local InfoBar_BackgroundColor = Color(80, 80, 80, 220)
+	local InfoBar_Color = Color(255, 255, 255, 255)
+	local InfoBar_Font = "Scoreboarder"
+	local InfoBar_BackgroundRoundness = 2
+	local InfoBar_BackgroundColor = Color(35, 35, 35, 56)
 
 	local 	Columns = {}
-			Columns[1] = {name="Name", command=function(self, arg) return tostring(arg:Name()) end}
-			Columns[2] = {name="Team", command=function(self, arg) return tostring(arg:Team()) end}
+			Columns[1] = {name="Player", command=function(self, arg) return tostring(arg:Name()) end}
+			Columns[2] = {name="KDR", command=function(self, arg) 
+			if tostring(arg:Frags()/arg:Deaths()) != "nan" && tostring(arg:Frags()/arg:Deaths()) != "inf" then 
+				return arg:Frags()/arg:Deaths()
+			else 
+				return "-" 
+			end 
+			end}
 			Columns[3] = {name="Ping", command=function(self, arg) return tostring(arg:Ping()) end}
 
 	surface.CreateFont("ScoreboardTitleFont", {
@@ -40,7 +46,23 @@ if CLIENT then
 		weight		= 1000,
 		antialias 	= true
 	})
-
+	surface.CreateFont("Scoreboarder",{
+		font = "Trebuchet24",
+		extended = false,
+		size = 24,
+		weight = 500,
+		blursize = 1.05,
+		scanlines = 2,
+		antialias = true,
+		underline = false,
+		italic = false,
+		strikeout = false,
+		symbol = false,
+		rotary = false,
+		shadow = false,
+		additive = false,
+		outline = false,
+	})
 	surface.CreateFont("ScoreboardInfoFont", {
 		font		= "CloseCaption_Normal",
 		size		= 28,
@@ -54,12 +76,27 @@ if CLIENT then
 		weight		= 500,
 		antialias 	= true
 	})
+	-- Thanks to slownls
+	DPANELBlurMat = Material("pp/blurscreen")
+	DPANELBlurMat2 = Material("pp/blurscreen")
 
+	function PanelDrawBlur(panel, amount) 
+		local tx, ty = panel:LocalToScreen(0, 0) 
+		local scrW, scrH = ScrW(), ScrH() 
+		surface.SetDrawColor(255, 255, 255) 
+		surface.SetMaterial(DPANELBlurMat) 
+		for i = 1, 3 do 
+			DPANELBlurMat:SetFloat("$blur", (i / 3) * (amount or 6)) 
+			DPANELBlurMat:Recompute() 
+			render.UpdateScreenEffectTexture() 
+			surface.DrawTexturedRect(tx * -1, ty * -1, scrW, scrH) 
+		end
+	end
 ----------LAUNCH SEQUENCE----------
 	local CreateScoreboard = function()
 		Scoreboard = vgui.Create("DFrame")
-		Scoreboard:SetSize(ScrW()*.75, ScrH()*.75)
-		Scoreboard:SetPos((ScrW()*.25)*.5, (ScrH()*.25)*.5)
+		Scoreboard:SetSize(ScrW()*.65, ScrH()*.65)
+		Scoreboard:SetPos((ScrW()*.1745), (ScrH()*.30)*.5)
 		Scoreboard:SetTitle("")
 		Scoreboard:SetDraggable(false)
 		Scoreboard:ShowCloseButton(false)
@@ -69,7 +106,8 @@ if CLIENT then
 		Scoreboard.Close = function(self)
 			Scoreboard:SetVisible(false)
 		end
-		Scoreboard.Paint = function(self) 
+		Scoreboard.Paint = function(self)
+			PanelDrawBlur(self, 5)
 			draw.RoundedBox(Scoreboard_Roundness, 0, 0, self:GetWide(), self:GetTall(), Scoreboard_Color)
 		end
 		 
@@ -81,6 +119,7 @@ if CLIENT then
 		local Height = h+(Title_Height*2)
 		Scoreboard.TitlePanel:SetSize(Scoreboard:GetWide()-(Scoreboard_XGap*2), Height)
 		Scoreboard.TitlePanel.Paint = function(self)
+
 			draw.RoundedBox(Title_BackgroundRoundness, 0, 0, self:GetWide(), self:GetTall(), Title_BackgroundColor)
 			surface.SetFont(Title_Font)
 			surface.SetTextColor(Title_Color.r, Title_Color.g, Title_Color.b, Title_Color.a)
@@ -111,7 +150,9 @@ if CLIENT then
 				self.PlayerBars[ID]:SetPos(0, 0)
 				self.PlayerBars[ID]:SetSize(Scoreboard.NamesListPanel:GetWide()-(Players_Spacing*2), PlayerBar_Height)
 				self.PlayerBars[ID].Paint = function(self)
-					draw.RoundedBox(PlayerBar_BackgroundRoundness, 0, 0, self:GetWide(), self:GetTall(), PlayerBar_BackgroundColor)
+					local PLYCOLORCONV = team.GetColor(pl:Team())
+					PLYCOLORCONV.a = 55
+					draw.RoundedBox(PlayerBar_BackgroundRoundness, 0, 0, self:GetWide(), self:GetTall(), PLYCOLORCONV)
 
 					surface.SetFont(PlayerBar_Font)
 					surface.SetTextColor(PlayerBar_Color.r, PlayerBar_Color.g, PlayerBar_Color.b, PlayerBar_Color.a)
@@ -152,7 +193,7 @@ if CLIENT then
 			for k, v in pairs(Columns) do
 				local w, h = surface.GetTextSize(v.name)
 				if k==1 then
-					surface.SetTextPos((ColumnGap_Half*k)-(w*.5), self:GetTall()*.5-(h*.5))
+					surface.SetTextPos((ColumnGap_Half*k)-(w*.41), self:GetTall()*.5-(h*.5))
 				else
 					surface.SetTextPos((ColumnGap_Width*(k-1))+(ColumnGap_Half)-(w*.5), self:GetTall()*.5-(h*.5))
 				end
